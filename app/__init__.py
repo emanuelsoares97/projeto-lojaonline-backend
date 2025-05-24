dd93930c1cb61522e88ebbe71109e7ca65b26b17
emanuelsoares97 <tutasoares17@gmail.com>
2025-05-24 19:06:04 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
-from config import Config
+from config import get_config
 
-def create_app(config_class=Config):
+def create_app():
     """Função de fábrica da aplicação Flask"""
     app = Flask(__name__)
+    
+    # Carregar configuração baseada no ambiente
+    config_class = get_config()
     app.config.from_object(config_class)
     
     # Inicialização de extensões
